dd00e55811106f79ad9d4a667c866b3b7a54ba29
surenGit <surenale69@gmail.com>
2022-06-23 21:24:32 +0545

    changing promises to async await procedure for post method

--- src/app.js
+++ src/app.js
@@ -5,30 +5,41 @@ const express = require('express');
 const app = express();
 
 //requiring the conn.js file to connect with mongoDb
- require("./db/conn");
+require("./db/conn");
 
- //require students.js for model extraction to main app page
- const Student = require("./models/students"); 
- 
+//require students.js for model extraction to main app page
+const Student = require("./models/students");
+
 
 const port = process.env.PORT || 8000;
 
+//recognizing json format api in express application
 app.use(express.json());
-//create a new students 
-app.post("/students", (req, res) => {
-    console.log(req.body);
-    const user = new Student(req.body);
-    user.save().then(() => {
-        res.status(201).send(user);
-    }).catch((e) => {
+
+//create a new students using promises
+// app.post("/students", (req, res) => {
+//     console.log(req.body);
+//     const user = new Student(req.body);
+//     user.save().then(() => {
+//         res.status(201).send(user);
+//     }).catch((e) => {
+//         res.status(400).send(e);
+//     })
+// })
+
+// create a new students using async await
+app.post("/students", async (req, res) => {
+    try {
+        const user = new Student(req.body);
+        const createUser = await user.save();
+        res.status(201).send(createUser);
+
+    } catch (e) {
         res.status(400).send(e);
-    })
-    //res.send("hello from the other side");
+    }
 })
-
-
 //for respose back to the user server should be listened
 // so defining port for listening
-app.listen(port, function(){
+app.listen(port, function () {
     console.log(`listening to port at ${port}`)
 })
